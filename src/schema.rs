7eef06639cd5b3f8e3b3f39348567074d6ab9113
Tom Planche <Tomplanche@icloud.com>
2025-05-01 17:47:06 +0200

    [20] (fix on switch-to-postgres) Better migrations + types for DB.

--- src/schema.rs
+++ src/schema.rs
@@ -1,18 +1,24 @@
-use diesel::table;
+// @generated automatically by Diesel CLI.
 
-table! {
+diesel::table! {
     transit_stop (id, route_id) {
+        #[max_length = 50]
         id -> Varchar,
+        #[max_length = 50]
         route_id -> Varchar,
+        #[max_length = 50]
         route_long_name -> Varchar,
+        #[max_length = 100]
         stop_name -> Varchar,
-        stop_lon -> Float8, // = f64
-        stop_lat -> Float8,
+        stop_lon -> Float4,
+        stop_lat -> Float4,
+        #[max_length = 20]
         shortname -> Varchar,
+        #[max_length = 100]
         nom_commune -> Varchar,
+        #[max_length = 10]
         code_insee -> Varchar,
+        #[max_length = 50]
         mode -> Varchar,
-        created_at -> Bigint, // = i64
-        updated_at -> Bigint,
     }
 }
