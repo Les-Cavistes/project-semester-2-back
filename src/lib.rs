b46cb9dbc00febad324cb3de54263535924dfaff
Tom Planche <tomplanche@proton.me>
2025-05-06 17:05:47 +0200

    [47] (feat on ratp-api-wrapper) Added `Url` struct and `RatpClient` struct.

--- src/lib.rs
+++ src/lib.rs
@@ -7,8 +7,10 @@ pub struct DbConn(diesel::PgConnection);
 pub mod api_response;
 pub mod models;
 pub mod paginated;
+pub mod ratp;
 pub mod routes;
 pub mod schema;
+pub mod url;
 
 pub const MAX_PER_PAGE: i64 = 100; // Prevent excessive page sizes
 pub const DEFAULT_PER_PAGE: i64 = 10;
