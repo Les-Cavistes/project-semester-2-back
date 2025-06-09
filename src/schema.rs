409c121097ed06bb6aac99913ebd8ec96b00b7f8
Glacoon <guillian.celle@gmail.com>
2025-06-09 02:00:26 +0200

    feat/include geojson: includes the GeoJSON part in the journey

--- src/schema.rs
+++ src/schema.rs
@@ -1,6 +1,14 @@
 // @generated automatically by Diesel CLI.
 
 diesel::table! {
+    tasks (id) {
+        id -> Int4,
+        description -> Varchar,
+        completed -> Bool,
+    }
+}
+
+diesel::table! {
     transit_stop (id, route_id) {
         #[max_length = 50]
         id -> Varchar,
@@ -22,3 +30,5 @@ diesel::table! {
         mode -> Varchar,
     }
 }
+
+diesel::allow_tables_to_appear_in_same_query!(tasks, transit_stop,);
